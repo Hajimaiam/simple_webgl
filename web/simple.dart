aaf33e9abe6e1990937279c14eb7c2c74f254e33
Hajimaiam <pkazda@gmail.com>
2014-11-11 01:54:21 -0500

    Update simple.dart

--- web/simple.dart
+++ web/simple.dart
@@ -25,6 +25,7 @@ class SimpleScene {
     _initGeometry();
     
     _gl.enable(webgl.DEPTH_TEST);
+    _gl.depthFunc(webgl.LESS);
     _gl.clearColor(0, 0, 0, 1);
     _gl.viewport(0, 0, _width, _height);
   }
@@ -63,18 +64,18 @@ void main() {
     var squPos = new Vector3List.view(buffer, 6*3, 6);
     var squCol = new Vector3List.view(buffer, 6*3+3, 6);
     // triangle vertices
-    triPos[0] = new Vector3( 0.0,  1.0, -0.5);
-    triPos[1] = new Vector3(-1.0, -1.0, -0.5);
-    triPos[2] = new Vector3( 1.0, -1.0, -0.5);
-    // triangle verticies colors
+    triPos[0] = new Vector3( 0.0,  1.0, -1.0);
+    triPos[1] = new Vector3(-1.0, -1.0, -1.0);
+    triPos[2] = new Vector3( 1.0, -1.0, -1.0);
+    // triangle vertices colors
     triCol[0] = new Vector3(0.0, 1.0, 0.0);
     triCol[1] = new Vector3(0.0, 1.0, 0.0);
     triCol[2] = new Vector3(0.0, 1.0, 0.0);
     // square vertices
-    squPos[0] = new Vector3( 0.5,  0.5, -1.0);
-    squPos[1] = new Vector3(-0.5,  0.5, -1.0);
-    squPos[2] = new Vector3( 0.5, -0.5, -1.0);
-    squPos[3] = new Vector3(-0.5, -0.5, -1.0);
+    squPos[0] = new Vector3( 0.5,  0.5, -0.5);
+    squPos[1] = new Vector3(-0.5,  0.5, -0.5);
+    squPos[2] = new Vector3( 0.5, -0.5, -0.5);
+    squPos[3] = new Vector3(-0.5, -0.5, -0.5);
     // square vertices colors
     squCol[0] = new Vector3(1.0, 1.0, 1.0);
     squCol[1] = new Vector3(1.0, 0.0, 0.0);
@@ -98,13 +99,19 @@ void main() {
     _gl.uniformMatrix4fv(_shader['uProjection'], false, _projection.storage);
     
     _gl.bindBuffer(webgl.ARRAY_BUFFER, _vbo);
+    _gl.clear(webgl.COLOR_BUFFER_BIT | webgl.DEPTH_BUFFER_BIT);
+    
     _gl.vertexAttribPointer(0, 3, webgl.FLOAT, false, 4*6, 4*0);
     _gl.vertexAttribPointer(1, 3, webgl.FLOAT, false, 4*6, 4*3);
     _gl.enableVertexAttribArray(0);
     _gl.enableVertexAttribArray(1);
-    _gl.drawArrays(webgl.TRIANGLES, 0, 3);
+    var elements = new Float32List(3);
+    elements = [0, 1, 2];
+    _gl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, elements, webgl.STATIC_DRAW);
+    _gl.drawElements(webgl.TRIANGLES, 3, webgl.FLOAT, 0);
+    //_gl.drawArrays(webgl.TRIANGLES, 0, 3);
     
-    _gl.drawArrays(webgl.TRIANGLE_STRIP, 3, 4);
+    //_gl.drawArrays(webgl.TRIANGLE_STRIP, 3, 4);
   }
 }
 
